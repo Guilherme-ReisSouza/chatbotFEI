5e1c55e591ffb3653f87317cb1f547d65209d8cb
Luiz Henrique <61383129+WolWolffz@users.noreply.github.com>
2024-03-06 17:42:27 -0300

    Update main old.py

--- Chatbot/main old.py
+++ Chatbot/main old.py
@@ -6,21 +6,39 @@ myChatBot = ChatBot()
 #criar o modelo
 myChatBot.createModel()
 
+def chatDebug():
+    myChatBot.setDebug(True)
+    print("Bem vindo ao Chatbot DEBUG")
 
-
-
-print("Bem vindo ao Chatbot")
-
-
-pergunta = input("como posso te ajudar?\n")
-resposta, intencao = myChatBot.chatbot_response(pergunta)
-# print(intencao)
-print(resposta)
-
-
-while (not intencao or (intencao and intencao[0]['intent']!="despedida")):
-    pergunta = input("posso lhe ajudar com algo a mais?\n")
+    pergunta = input("como posso te ajudar?\n")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
+    # print(intencao)
     print(resposta)
 
-print("Foi um prazer atendê-lo")
+    while (not intencao or (intencao and intencao[0]['intent']!="despedida")):
+        pergunta = input("posso lhe ajudar com algo a mais?\n")
+        resposta, intencao = myChatBot.chatbot_response(pergunta)
+        print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+    print("Foi um prazer atendê-lo")
+    
+def chatFinal():
+    print("Olá! Seja muito bem vindo ao assistente PIPE! Em que posso ajudar?\n")
+
+    pergunta = input()
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+
+    print(resposta + "\n")
+
+    while (not intencao or (intencao and intencao[0]['intent']!="despedida")):
+        pergunta = input()
+        resposta, intencao = myChatBot.chatbot_response(pergunta)
+        print(resposta + "   [" + intencao[0]['intent'] + "]")
+        print("\n")
+    
+    print("Espero que eu tenha sido de boa ajuda :) Até breve!")
+    
+#descomente o que deseja utilizar, comentando o outro
+
+chatFinal()
+#chatDebug()
